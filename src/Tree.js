78eea9ca5ef1d8b1a5ca77f4c067c4f8a2e2af5c
kurian.thomas <kurian.thomas@ASDLAPKCH0366.aspiresys.kl>
2023-10-06 15:47:43 +0530

    Added option to toggle drag feature

--- src/Tree.js
+++ src/Tree.js
@@ -6,9 +6,9 @@
 // };
 
 // export default Tree;
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const TreeNode = ({ node, onDragStart, onDragOver, onDrop }) => {
+const TreeNode = ({ node, dragEnabled, onDragStart, onDragOver, onDrop }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const handleToggle = () => {
@@ -17,19 +17,27 @@ const TreeNode = ({ node, onDragStart, onDragOver, onDrop }) => {
 
   return (
     <div
-      draggable
+      draggable={dragEnabled}
       onDragStart={(e) => onDragStart(e, node)}
       onDragOver={(e) => onDragOver(e, node)}
       onDrop={(e) => onDrop(e, node)}
     >
       <div onClick={handleToggle}>
-        {node.children && node.children.length > 0 && (isExpanded ? 'V ' : '> ')}
+        {node.children &&
+          node.children.length > 0 &&
+          (isExpanded ? "V " : "> ")}
         {node.label}
       </div>
       {isExpanded && node.children && (
-        <div style={{ marginLeft: '20px' }}>
+        <div style={{ marginLeft: "20px" }}>
           {node.children.map((childNode) => (
-            <TreeNode key={childNode.id} node={childNode} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
+            <TreeNode
+              key={childNode.id}
+              node={childNode}
+              onDragStart={onDragStart}
+              onDragOver={onDragOver}
+              onDrop={onDrop}
+            />
           ))}
         </div>
       )}
@@ -50,8 +58,12 @@ const Tree = ({ data }) => {
 
   const handleDrop = (e, targetNode) => {
     const updatedData = [...data];
-    const dropIndex = updatedData.findIndex((node) => node.id === targetNode.id);
-    const draggedIndex = updatedData.findIndex((node) => node.id === draggedItem.id);
+    const dropIndex = updatedData.findIndex(
+      (node) => node.id === targetNode.id
+    );
+    const draggedIndex = updatedData.findIndex(
+      (node) => node.id === draggedItem.id
+    );
 
     // Move the dragged item to the new position
     updatedData.splice(draggedIndex, 1);
@@ -65,11 +77,16 @@ const Tree = ({ data }) => {
   return (
     <div>
       {data.map((node) => (
-        <TreeNode key={node.id} node={node} onDragStart={handleDragStart} onDragOver={handleDragOver} onDrop={handleDrop} />
+        <TreeNode
+          key={node.id}
+          node={node}
+          onDragStart={handleDragStart}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        />
       ))}
     </div>
   );
 };
 
 export default Tree;
-
