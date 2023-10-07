77864e104184d934e4f3febfe3a2efd6000b007e
Dilna K Daison <dilna.daison@aspiresys.com>
2023-10-07 23:47:58 +0530

    Creation of tree library with draggable

--- src/App.js
+++ src/App.js
@@ -1,36 +1,245 @@
-import React from 'react';
+// import React from 'react';
+// import Tree from './Tree';
+
+// const treeData = [
+//   {
+//     id: 1,
+//     label: 'Node 1',
+//     children: [
+//       {
+//         id: 2,
+//         label: 'Node 1.1',
+//         children: [
+//           {
+//             id: 3,
+//             label: 'Node 1.1.1',
+//             children: [],
+//           },
+//         ],
+//       },
+//       {
+//         id: 4,
+//         label: 'Node 1.2',
+//         children: [],
+//       },
+//     ],
+//   },
+// ];
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Tree Library Example</h1>
+//       <Tree data={treeData} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+// import React from 'react';
+// import Tree from './Tree';
+// import "./App.css"
+// const jsonData = [
+//   {
+//     id: 1,
+//     name: 'Node 1',
+//     children: [
+//       {
+//         id: 2,
+//         name: 'Node 1.1',
+//         children: [
+//           {
+//             id: 3,
+//             name: 'Node 1.1.1',
+//           },
+//         ],
+//       },
+//       {
+//         id: 4,
+//         name: 'Node 1.2',
+//       },
+//     ],
+//   },
+//   {
+//     id: 5,
+//     name: 'Node 2',
+//     children: [
+//       {
+//         id: 6,
+//         name: 'Node 2.1',
+//       },
+//     ],
+//   },
+// ];
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Tree Component with Drag and Drop</h1>
+//       <Tree data={jsonData} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+// import React, { useState } from 'react';
+// import Tree from './Tree';
+
+// const App = () => {
+//   const initialData = [
+//     {
+//       key: '1',
+//       title: 'Node 1',
+//       children: [
+//         {
+//           key: '1-1',
+//           title: 'Node 1.1',
+//           children: [],
+//         },
+//         {
+//           key: '1-2',
+//           title: 'Node 1.2',
+//           children: [],
+//         },
+//       ],
+//     },
+//     {
+//       key: '2',
+//       title: 'Node 2',
+//       children: [],
+//     },
+//   ];
+
+//   const [data, setData] = useState(initialData);
+
+//   const handleDragStart = (draggedNode, e) => {
+//     // Handle drag start event
+//   };
+
+//   const handleDragOver = (targetNode, e) => {
+//     // Handle drag over event
+//   };
+
+//   const handleDrop = (targetNode, e) => {
+//     // Handle drop event and update the tree structure
+//   };
+
+//   return (
+//     <div>
+//       <h1>Draggable Tree</h1>
+//       <Tree data={data} onDragStart={handleDragStart} onDragOver={handleDragOver} onDrop={handleDrop} />
+//     </div>
+//   );
+// };
+
+// export default App;
+import React, { useState } from 'react';
 import Tree from './Tree';
 
-const treeData = [
-  {
-    id: 1,
-    label: 'Node 1',
-    children: [
-      {
-        id: 2,
-        label: 'Node 1.1',
-        children: [
-          {
-            id: 3,
-            label: 'Node 1.1.1',
-            children: [],
-          },
-        ],
-      },
-      {
-        id: 4,
-        label: 'Node 1.2',
-        children: [],
-      },
-    ],
-  },
-];
-
 const App = () => {
+  const initialData = [
+    {
+      key: '1',
+      title: 'Node 1',
+      children: [
+        {
+          key: '1-1',
+          title: 'Node 1.1',
+          children: [],
+        },
+        {
+          key: '1-2',
+          title: 'Node 1.2',
+          children: [],
+        },
+      ],
+    },
+    {
+      key: '2',
+      title: 'Node 2',
+      children: [],
+    },
+  ];
+
+  const [data, setData] = useState(initialData);
+
+  const handleDragStart = (draggedNode, e) => {
+    // Store the dragged node for later use
+    e.dataTransfer.setData('text/plain', JSON.stringify(draggedNode));
+  };
+
+  const handleDragOver = (targetNode, e) => {
+    // Prevent default behavior to allow drop
+    e.preventDefault();
+  };
+
+  const handleDrop = (targetNode, e) => {
+    // Prevent the default drop behavior
+    e.preventDefault();
+
+    // Get the dragged node data from the data transfer object
+    const draggedNode = JSON.parse(e.dataTransfer.getData('text/plain'));
+
+    // Clone the current data state to avoid mutating the state directly
+    const newData = [...data];
+
+    // Find the target node in the tree
+    const findNode = (nodes, key) => {
+      for (let node of nodes) {
+        if (node.key === key) {
+          return node;
+        }
+        if (node.children && node.children.length > 0) {
+          const foundNode = findNode(node.children, key);
+          if (foundNode) {
+            return foundNode;
+          }
+        }
+      }
+      return null;
+    };
+
+    // Find the dragged node in the tree
+    const sourceNode = findNode(newData, draggedNode.key);
+
+    // Remove the dragged node from its original position
+    const removeNode = (nodes, key) => {
+      for (let i = 0; i < nodes.length; i++) {
+        if (nodes[i].key === key) {
+          nodes.splice(i, 1);
+          return true;
+        }
+        if (nodes[i].children && nodes[i].children.length > 0) {
+          const nodeRemoved = removeNode(nodes[i].children, key);
+          if (nodeRemoved) return true;
+        }
+      }
+      return false;
+    };
+
+    // Remove the dragged node from its original position
+    removeNode(newData, draggedNode.key);
+
+    // Add the dragged node to the target node's children
+    if (!targetNode.children) {
+      targetNode.children = [];
+    }
+
+    targetNode.children.push(sourceNode);
+
+    // Update the state with the new tree structure
+    setData(newData);
+  };
+
   return (
     <div>
-      <h1>Tree Library Example</h1>
-      <Tree data={treeData} />
+      <h1>Draggable Tree</h1>
+      <Tree data={data} onDragStart={handleDragStart} onDragOver={handleDragOver} onDrop={handleDrop} />
     </div>
   );
 };
--- src/Tree.js
+++ src/Tree.js
@@ -6,66 +6,176 @@
 // };
 
 // export default Tree;
+
+
+// import React, { useState } from 'react';
+
+// const TreeNode = ({ node, onDragStart, onDragOver, onDrop }) => {
+//   const [isExpanded, setIsExpanded] = useState(false);
+
+//   const handleToggle = () => {
+//     setIsExpanded(!isExpanded);
+//   };
+
+//   return (
+//     <div
+//       draggable
+//       onDragStart={(e) => onDragStart(e, node)}
+//       onDragOver={(e) => onDragOver(e, node)}
+//       onDrop={(e) => onDrop(e, node)}
+//     >
+//       <div onClick={handleToggle}>
+//         {node.children && node.children.length > 0 && (isExpanded ? 'V ' : '> ')}
+//         {node.label}
+//       </div>
+//       {isExpanded && node.children && (
+//         <div style={{ marginLeft: '20px' }}>
+//           {node.children.map((childNode) => (
+//             <TreeNode key={childNode.id} node={childNode} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// const Tree = ({ data }) => {
+//   const [draggedItem, setDraggedItem] = useState(null);
+
+//   const handleDragStart = (e, node) => {
+//     setDraggedItem(node);
+//   };
+
+//   const handleDragOver = (e, node) => {
+//     e.preventDefault();
+//   };
+
+//   const handleDrop = (e, targetNode) => {
+//     const updatedData = [...data];
+//     const dropIndex = updatedData.findIndex((node) => node.id === targetNode.id);
+//     const draggedIndex = updatedData.findIndex((node) => node.id === draggedItem.id);
+
+//     // Move the dragged item to the new position
+//     updatedData.splice(draggedIndex, 1);
+//     updatedData.splice(dropIndex, 0, draggedItem);
+
+//     // Update the state with the new data
+//     setDraggedItem(null);
+//     // TODO: Update your state with updatedData
+//   };
+
+//   return (
+//     <div>
+//       {data.map((node) => (
+//         <TreeNode key={node.id} node={node} onDragStart={handleDragStart} onDragOver={handleDragOver} onDrop={handleDrop} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Tree;
+
+
+
+// import React from 'react';
+// import TreeNode from './TreeNode';
+
+// const Tree = ({ data, onDragStart, onDragOver, onDrop }) => {
+//   return (
+//     <div className="tree">
+//       {data.map((node) => (
+//         <TreeNode key={node.key} node={node} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Tree;
+
+// import React, { useState } from 'react';
+
+// const TreeNode = ({ node, onDragStart, onDragOver, onDrop }) => {
+//       const [isExpanded, setIsExpanded] = useState(false);
+
+//   const handleToggle = () => {
+//     setIsExpanded(!isExpanded);
+//   };
+//   return (
+//     <div
+//       draggable
+//       onDragStart={(e) => onDragStart(node, e)}
+//       onDragOver={(e) => onDragOver(node, e)}
+//       onDrop={(e) => onDrop(node, e)}
+//     >
+//       {node.title}
+//       {node.children && node.children.length > 0 && (
+//         <div style={{ marginLeft: '20px' }}>
+//           {node.children.map((child) => (
+//             <TreeNode key={child.key} node={child} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// const Tree = ({ data, onDragStart, onDragOver, onDrop }) => {
+//   return (
+//     <div>
+//       {data.map((node) => (
+//         <TreeNode key={node.key} node={node} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Tree;
 import React, { useState } from 'react';
 
 const TreeNode = ({ node, onDragStart, onDragOver, onDrop }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-  const handleToggle = () => {
-    setIsExpanded(!isExpanded);
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
   };
 
   return (
     <div
       draggable
-      onDragStart={(e) => onDragStart(e, node)}
-      onDragOver={(e) => onDragOver(e, node)}
-      onDrop={(e) => onDrop(e, node)}
+      onDragStart={(e) => onDragStart(node, e)}
+      onDragOver={(e) => onDragOver(node, e)}
+      onDrop={(e) => onDrop(node, e)}
     >
-      <div onClick={handleToggle}>
-        {node.children && node.children.length > 0 && (isExpanded ? 'V ' : '> ')}
-        {node.label}
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span onClick={toggleDropdown} style={{ cursor: 'pointer', marginRight: '8px' }}>
+          {isDropdownOpen ? 'v' : '>'}
+        </span>
+        {node.title}
       </div>
-      {isExpanded && node.children && (
+      {isDropdownOpen && (
         <div style={{ marginLeft: '20px' }}>
-          {node.children.map((childNode) => (
-            <TreeNode key={childNode.id} node={childNode} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
-          ))}
+          {node.children &&
+            node.children.length > 0 &&
+            node.children.map((child) => (
+              <TreeNode
+                key={child.key}
+                node={child}
+                onDragStart={onDragStart}
+                onDragOver={onDragOver}
+                onDrop={onDrop}
+              />
+            ))}
         </div>
       )}
     </div>
   );
 };
 
-const Tree = ({ data }) => {
-  const [draggedItem, setDraggedItem] = useState(null);
-
-  const handleDragStart = (e, node) => {
-    setDraggedItem(node);
-  };
-
-  const handleDragOver = (e, node) => {
-    e.preventDefault();
-  };
-
-  const handleDrop = (e, targetNode) => {
-    const updatedData = [...data];
-    const dropIndex = updatedData.findIndex((node) => node.id === targetNode.id);
-    const draggedIndex = updatedData.findIndex((node) => node.id === draggedItem.id);
-
-    // Move the dragged item to the new position
-    updatedData.splice(draggedIndex, 1);
-    updatedData.splice(dropIndex, 0, draggedItem);
-
-    // Update the state with the new data
-    setDraggedItem(null);
-    // TODO: Update your state with updatedData
-  };
-
+const Tree = ({ data, onDragStart, onDragOver, onDrop }) => {
   return (
     <div>
       {data.map((node) => (
-        <TreeNode key={node.id} node={node} onDragStart={handleDragStart} onDragOver={handleDragOver} onDrop={handleDrop} />
+        <TreeNode key={node.key} node={node} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} />
       ))}
     </div>
   );
