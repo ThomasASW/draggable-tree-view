2d2462929ee947330c52464f14bdb506419ef0a7
ThomasASW <125454436+ThomasASW@users.noreply.github.com>
2023-10-15 13:25:15 +0530

    Add toggles for disabling drag, edit and delete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import Tree from "./Tree";
+import Tree from "./Tree";
 
 const App = () => {
   const initialData = [
@@ -93,6 +93,7 @@ const App = () => {
 
     setData(newData);
   };
+
   const handleEditNode = (key, newTitle) => {
     const editNode = (nodes) => {
       for (let i = 0; i < nodes.length; i++) {
@@ -188,147 +189,11 @@ const App = () => {
         onDrop={handleDrop}
         onDeleteNode={handleDeleteNode}
         onEditNode={handleEditNode}
+        dragEnabled={false}
+        readOnly={true}
       />
     </div>
   );
 };
-const TreeNode = ({
-  node,
-  onDragStart,
-  onDragOver,
-  onDrop,
-  onDelete,
-  onEditNode,
-}) => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-
-  const [isEditing, setEditing] = useState(false);
-  const [editedTitle, setEditedTitle] = useState(node.title);
-
-  const handleEditInputChange = (e) => {
-    setEditedTitle(e.target.value);
-  };
-
-  const handleEditSave = () => {
-    onEditNode(node.key, editedTitle);
-    setEditing(false);
-  };
-
-  return (
-    <div
-      draggable
-      onDragStart={(e) => {
-        console.log(node, "drag started");
-        onDragStart(node, e);
-      }}
-      onDragOver={(e) => onDragOver(node, e)}
-      onDrop={(e) => onDrop(node, e)}
-    >
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <span
-          onClick={toggleDropdown}
-          style={{ cursor: "pointer", marginRight: "10px" }}
-        >
-          {isEditing ? (
-            <div>
-              <input
-                type="text"
-                value={editedTitle}
-                onChange={handleEditInputChange}
-              />
-              <button onClick={handleEditSave}>Save</button>
-            </div>
-          ) : (
-            ""
-          )}
-
-          {node.children.length > 0 && isDropdownOpen ? (
-            <i
-              className="fa-solid fa-caret-down"
-              style={{ marginLeft: "10px" }}
-            ></i>
-          ) : (
-            // <i
-            //   className="fa-solid fa-caret-right"
-            //   style={{ marginLeft: "10px" }}
-            // ></i>
-            ""
-          )}
-
-          {node.children.length > 0 && !isDropdownOpen ? (
-            <i
-              className="fa-solid fa-caret-right"
-              style={{ marginLeft: "10px" }}
-            ></i>
-          ) : (
-            ""
-          )}
-          {node.children.length === 0 && !isDropdownOpen ? (
-            <i className="fa" style={{ marginLeft: "17px" }}></i>
-          ) : (
-            ""
-          )}
-        </span>
-        {node.title} &emsp;{" "}
-        <i
-          className="fa-solid fa-trash"
-          style={{ color: "grey" }}
-          onClick={() => onDelete(node.key)}
-        ></i>
-        &emsp;
-        <i
-          className="fas fa-edit"
-          style={{ color: "dark grey" }}
-          onClick={() => setEditing(true)}
-        ></i>
-      </div>
-      {isDropdownOpen && (
-        <div style={{ marginLeft: "20px" }}>
-          {node.children &&
-            node.children.length > 0 &&
-            node.children.map((child) => (
-              <TreeNode
-                key={child.key}
-                node={child}
-                onDragStart={onDragStart}
-                onDragOver={onDragOver}
-                onDrop={onDrop}
-                onDelete={onDelete}
-                onEditNode={onEditNode}
-              />
-            ))}
-        </div>
-      )}
-    </div>
-  );
-};
-
-const Tree = ({
-  data,
-  onDragStart,
-  onDragOver,
-  onDrop,
-  onDeleteNode,
-  onEditNode,
-}) => {
-  return (
-    <div>
-      {data.map((node) => (
-        <TreeNode
-          key={node.key}
-          node={node}
-          onDragStart={onDragStart}
-          onDragOver={onDragOver}
-          onDrop={onDrop}
-          onDelete={onDeleteNode}
-          onEditNode={onEditNode}
-        />
-      ))}
-    </div>
-  );
-};
 export default App;
--- src/Tree.js
+++ src/Tree.js
@@ -1,130 +1,157 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const TreeNode = ({
-//   node,
-//   onDragStart,
-//   onDragOver,
-//   onDrop,
-//   onDelete,
-//   onEditNode,
-// }) => {
-//   const [isDropdownOpen, setDropdownOpen] = useState(false);
+const TreeNode = ({
+  node,
+  onDragStart,
+  onDragOver,
+  onDrop,
+  onDelete,
+  onEditNode,
+  dragEnabled,
+  readOnly,
+}) => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-//   const toggleDropdown = () => {
-//     setDropdownOpen(!isDropdownOpen);
-//   };
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
-//   const [isEditing, setEditing] = useState(false);
-//   const [editedTitle, setEditedTitle] = useState(node.title);
+  const [isEditing, setEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(node.title);
 
-//   const handleEditInputChange = (e) => {
-//     setEditedTitle(e.target.value);
-//   };
+  const handleEditInputChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
 
-//   const handleEditSave = () => {
-//     onEditNode(node.key, editedTitle);
-//     setEditing(false);
-//   };
+  const handleEditSave = () => {
+    onEditNode(node.key, editedTitle);
+    setEditing(false);
+  };
 
-//   return (
-//     <div
-//       draggable
-//       onDragStart={(e) => {
-//         console.log(node, "drag started");
-//         onDragStart(node, e);
-//       }}
-//       onDragOver={(e) => onDragOver(node, e)}
-//       onDrop={(e) => onDrop(node, e)}
-//     >
-//       <div style={{ display: "flex", alignItems: "center" }}>
-//         <span
-//           onClick={toggleDropdown}
-//           style={{ cursor: "pointer", marginRight: "10px" }}
-//         >
-//           {isEditing ? (
-//             <div>
-//               <input
-//                 type="text"
-//                 value={editedTitle}
-//                 onChange={handleEditInputChange}
-//               />
-//               <button onClick={handleEditSave}>Save</button>
-//             </div>
-//           ) : (
-//             ""
-//           )}
+  return (
+    <div
+      draggable={dragEnabled}
+      onDragStart={(e) => {
+        console.log(node, "drag started");
+        onDragStart(node, e);
+      }}
+      onDragOver={(e) => onDragOver(node, e)}
+      onDrop={(e) => onDrop(node, e)}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span
+          onClick={toggleDropdown}
+          style={{ cursor: "pointer", marginRight: "10px" }}
+        >
+          {isEditing ? (
+            <div>
+              <input
+                type="text"
+                value={editedTitle}
+                onChange={handleEditInputChange}
+              />
+              <button onClick={handleEditSave}>Save</button>
+            </div>
+          ) : (
+            ""
+          )}
 
-//           {isDropdownOpen ? (
-//             <i
-//               className="fa-solid fa-caret-down"
-//               style={{ marginLeft: "10px" }}
-//             ></i>
-            
-//           ) : (
-//             <i
-//               className="fa-solid fa-caret-right"
-//               style={{ marginLeft: "10px" }}
-//             ></i>
-//           )}
-//         </span>
-//         {node.title} &emsp;{" "}
-//         <i
-//           className="fa-solid fa-trash"
-//           style={{ color: "grey" }}
-          
-//           onClick={() => onDelete(node.key)}
-//         ></i>
-//         &emsp;
-//         <i
-//           className="fas fa-edit"
-//           style={{ color: "dark grey" }}
-//           onClick={() =>setEditing(true)}
-//         ></i>
-//       </div>
-//       {isDropdownOpen && (
-//         <div style={{ marginLeft: "20px" }}>
-//           {node.children &&
-//             node.children.length > 0 &&
-//             node.children.map((child) => (
-//               <TreeNode
-//                 key={child.key}
-//                 node={child}
-//                 onDragStart={onDragStart}
-//                 onDragOver={onDragOver}
-//                 onDrop={onDrop}
-//                 onDelete={onDelete}
-//                 onEditNode={onEditNode}
-//               />
-//             ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+          {node.children.length > 0 && isDropdownOpen ? (
+            <i
+              className="fa-solid fa-caret-down"
+              style={{ marginLeft: "10px" }}
+            ></i>
+          ) : (
+            // <i
+            //   className="fa-solid fa-caret-right"
+            //   style={{ marginLeft: "10px" }}
+            // ></i>
+            ""
+          )}
 
-// const Tree = ({
-//   data,
-//   onDragStart,
-//   onDragOver,
-//   onDrop,
-//   onDeleteNode,
-//   onEditNode,
-// }) => {
-//   return (
-//     <div>
-//       {data.map((node) => (
-//         <TreeNode
-//           key={node.key}
-//           node={node}
-//           onDragStart={onDragStart}
-//           onDragOver={onDragOver}
-//           onDrop={onDrop}
-//           onDelete={onDeleteNode}
-//           onEditNode={onEditNode}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
+          {node.children.length > 0 && !isDropdownOpen ? (
+            <i
+              className="fa-solid fa-caret-right"
+              style={{ marginLeft: "10px" }}
+            ></i>
+          ) : (
+            ""
+          )}
+          {node.children.length === 0 && !isDropdownOpen ? (
+            <i className="fa" style={{ marginLeft: "17px" }}></i>
+          ) : (
+            ""
+          )}
+        </span>
+        {node.title} &emsp;{" "}
+        {readOnly ? (
+          <></>
+        ) : (
+          <>
+            <i
+              className="fa-solid fa-trash"
+              style={{ color: "grey" }}
+              onClick={() => onDelete(node.key)}
+            ></i>
+            &emsp;
+            <i
+              className="fas fa-edit"
+              style={{ color: "dark grey" }}
+              onClick={() => setEditing(true)}
+            ></i>
+          </>
+        )}
+      </div>
+      {isDropdownOpen && (
+        <div style={{ marginLeft: "20px" }}>
+          {node.children &&
+            node.children.length > 0 &&
+            node.children.map((child) => (
+              <TreeNode
+                key={child.key}
+                node={child}
+                onDragStart={onDragStart}
+                onDragOver={onDragOver}
+                onDrop={onDrop}
+                onDelete={onDelete}
+                onEditNode={onEditNode}
+                dragEnabled={dragEnabled}
+                readOnly={readOnly}
+              />
+            ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-// export default Tree;
+const Tree = ({
+  data,
+  onDragStart,
+  onDragOver,
+  onDrop,
+  onDeleteNode,
+  onEditNode,
+  dragEnabled,
+  readOnly,
+}) => {
+  return (
+    <div>
+      {data.map((node) => (
+        <TreeNode
+          key={node.key}
+          node={node}
+          onDragStart={onDragStart}
+          onDragOver={onDragOver}
+          onDrop={onDrop}
+          onDelete={onDeleteNode}
+          onEditNode={onEditNode}
+          dragEnabled={dragEnabled}
+          readOnly={readOnly}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Tree;
